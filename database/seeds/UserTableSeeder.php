b39f2e6f03210af4fd655ec5b4f4f33f08b651f3
Anthony <martab7.am@gmail.com>
2016-08-22 11:39:50 -0500

    soME updates

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,30 +13,30 @@ class UserTableSeeder extends Seeder
     public function run()
     {
 
-      factory(Post::class, 100)->create();
+      // factory(Post::class, 100)->create();
 
-    //   $users = [
-    //   ['email'=>'anthony@codeup.com', 'name'=>'Anthony', 'password'=>'Anthony'],
-    //   ['email'=>'luis@codeup.com', 'name'=>'Luis', 'password'=>'Luis'],
-    //   ['email'=>'cameron@codeup.com', 'name'=>'Cameron', 'password'=>'Cameron'],
-    //   ['email'=>'tyler@codeup.com', 'name'=>'Tyler', 'password'=>'Tyler'],
-    //   ['email'=>'scott@codeup.com', 'name'=>'Scott', 'password'=>'Scott'],
-    //   ['email'=>'emily@codeup.com', 'name'=>'Emily', 'password'=>'Emily'],
-    //   ['email'=>'anna@codeup.com', 'name'=>'Anna', 'password'=>'Anna'],
-    //   ['email'=>'phillip@codeup.com', 'name'=>'Phillip', 'password'=>'Phillip'],
-    //   ['email'=>'john@codeup.com', 'name'=>'John', 'password'=>'John'],
-    //   ['email'=>'craig@codeup.com', 'name'=>'Craig', 'password'=>'Craig'],
-    //   ['email'=>'brandon@codeup.com', 'name'=>'Brandon', 'password'=>'Brandon']
-    // ];
-    //
-    //   foreach($users as $user){
-    //     // $user1 = new App\User();
-    //     // $user1->email = $user['email'];
-    //     // $user1->name = $user['name'];
-    //     // $user1->password = Hash::make($user['password']);
-    //     // $user1->save();
-    //     User::create();
-    //   }
+      $users = [
+      ['email'=>'anthony@codeup.com', 'name'=>'Anthony', 'password'=>'Anthony'],
+      ['email'=>'luis@codeup.com', 'name'=>'Luis', 'password'=>'Luis'],
+      ['email'=>'cameron@codeup.com', 'name'=>'Cameron', 'password'=>'Cameron'],
+      ['email'=>'tyler@codeup.com', 'name'=>'Tyler', 'password'=>'Tyler'],
+      ['email'=>'scott@codeup.com', 'name'=>'Scott', 'password'=>'Scott'],
+      ['email'=>'emily@codeup.com', 'name'=>'Emily', 'password'=>'Emily'],
+      ['email'=>'anna@codeup.com', 'name'=>'Anna', 'password'=>'Anna'],
+      ['email'=>'phillip@codeup.com', 'name'=>'Phillip', 'password'=>'Phillip'],
+      ['email'=>'john@codeup.com', 'name'=>'John', 'password'=>'John'],
+      ['email'=>'craig@codeup.com', 'name'=>'Craig', 'password'=>'Craig'],
+      ['email'=>'brandon@codeup.com', 'name'=>'Brandon', 'password'=>'Brandon']
+    ];
+
+      foreach($users as $user){
+        // $user1 = new App\User();
+        // $user1->email = $user['email'];
+        // $user1->name = $user['name'];
+        // $user1->password = Hash::make($user['password']);
+        // $user1->save();
+        User::create($user);
+      }
 
     }
 }
